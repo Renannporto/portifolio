51aadbf5de7c285e449fab6124e42dd972409e89
Renannporto <144285350+Renannporto@users.noreply.github.com>
2025-07-22 22:03:07 +0100

    extilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,23 @@
+import Avatar from '../../components/Avatar'
+import { FotoPerfil } from '../../components/Avatar/styles'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/renannporto.png" />
-    <Titulo fontSize={20}>Renan Porto</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Renan Porto</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        renannporto
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
